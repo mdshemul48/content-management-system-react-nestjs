9d0b916c9de9396780c119cf15f56409b24fbb9b
MD. Shimul <mdshemul480@gmail.com>
2022-05-19 14:31:24 +0600

    fresh install with movies database

---
+++ database/migrations/2022_05_19_081354_create_movies_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('fullTitle');
+            $table->text('downloadLink');
+            $table->text('poster');
+            $table->string('tmdb_id')->nullable();
+            $table->year('year');
+            $table->string('quality');
+            $table->string('resolution');
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+};
