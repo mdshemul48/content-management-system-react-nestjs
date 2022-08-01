62c83b02983a97e1c02d2af3797bfe801c0266ee
MD. Shimul <mdshemul480@gmail.com>
2022-08-01 16:31:06 +0600

    adding dashboard page

--- client-side/src/App.jsx
+++ client-side/src/App.jsx
@@ -35,7 +35,7 @@ function App() {
 
             <Route path="/admin">
               <Route
-                index
+                path="dashboard"
                 element={
                   <RequireAuth>
                     <AdminHomePage />
--- client-side/src/Components/Shared/NavBar/AdminBar.jsx
+++ client-side/src/Components/Shared/NavBar/AdminBar.jsx
@@ -1,13 +1,42 @@
 import React from "react";
+
+import { Dropdown } from "react-bootstrap";
 import { FaUserAlt } from "react-icons/fa";
+import { Link, useNavigate } from "react-router-dom";
+
+import useUser from "../../../Hooks/useUser";
 
 function AdminBar() {
+  const navigate = useNavigate();
+
+  const { user, logout } = useUser();
+  console.log(user);
+
+  const logoutHandler = () => {
+    logout();
+    navigate("/", { replace: true });
+  };
+
   return (
-    <div>
-      {" "}
-      <FaUserAlt className="fs-4" />
-      gg
-    </div>
+    user && (
+      <Dropdown className="ms-2 me-5">
+        <Dropdown.Toggle variant="light" id="dropdown-basic">
+          <FaUserAlt className="fs-4 text-secondary" />
+          <span className="ms-1">{user?.user?.name}</span>
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          <Dropdown.Item as={Link} to="/admin/dashboard">
+            Dashboard
+          </Dropdown.Item>
+          <Dropdown.Item href="#">Add New Post</Dropdown.Item>
+          <Dropdown.Item href="#">Users</Dropdown.Item>
+          <Dropdown.Divider />
+
+          <Dropdown.Item onClick={logoutHandler}>Logout</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+    )
   );
 }
 
