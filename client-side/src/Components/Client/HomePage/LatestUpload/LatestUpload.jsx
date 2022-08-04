b6de0298997b3f0e1077640eb0edbf7639ea5b4e
MD. Shimul <mdshemul480@gmail.com>
2022-08-05 01:20:30 +0600

    adding arrow button

--- client-side/src/Components/Client/HomePage/LatestUpload/LatestUpload.jsx
+++ client-side/src/Components/Client/HomePage/LatestUpload/LatestUpload.jsx
@@ -1,13 +1,21 @@
 import React from "react";
-import { Container, Row } from "react-bootstrap";
+import { Button, Container, Row } from "react-bootstrap";
+import ArrowButton from "../../../Shared/ArrowButton/ArrowButton";
 // import SinglePost from "../../../Shared/SinglePost/SinglePost";
 
 function LatestUpload() {
   return (
     <Container className="mt-5" fluid>
-      <h2 className="text-white">Latest Upload</h2>
-      <hr />
-      <hr className="text-light" />
+      <div className="d-flex justify-content-between">
+        <h4 className="text-white fw-bold my-0">Latest Upload</h4>
+        <div>
+          <ArrowButton> {"<"} </ArrowButton>
+          <ArrowButton> {">"} </ArrowButton>
+        </div>
+      </div>
+
+      <hr className="m-0" />
+      <hr className="text-light my-1" />
       <Row className="mt-1">
         {/* <SinglePost />
         <SinglePost />
