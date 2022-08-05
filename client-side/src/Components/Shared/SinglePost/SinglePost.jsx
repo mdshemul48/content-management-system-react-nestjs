fe747c40512577b684c324b26d8556bb02676dcc
MD. Shimul <mdshemul480@gmail.com>
2022-08-05 22:42:55 +0600

    adding image forground

--- client-side/src/Components/Shared/SinglePost/SinglePost.jsx
+++ client-side/src/Components/Shared/SinglePost/SinglePost.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ButtonGroup, Col, ToggleButton } from "react-bootstrap";
+import { Button, ButtonGroup, Col, ToggleButton } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 import styles from "./SinglePost.module.css";
@@ -10,14 +10,21 @@ function SinglePost(props) {
   // } = props;
   return (
     <Col xxl={2}>
-      <div className="rounded" style={{ background: "#DDDDDD" }}>
+      <div className={`rounded ${styles.singlePost_card} p-1 m-1`}>
         <Link to="/content/2" className="text-decoration-none">
-          <div className="overflow-hidden">
+          <div className="overflow-hidden d-flex justify-content-center  align-items-end rounded">
             <img
-              src="https://streamo.vuejstemplate.com/images/product/movie-01.jpg"
-              className={`w-100 ${styles.singlePost_image}`}
+              src="https://streamo.vuejstemplate.com/images/product/comedy-series-1.png"
+              className={`w-100 ${styles.singlePost_image} rounded`}
               alt=""
             />
+            <div className={`text-center ${styles.singlePost_text}`}>
+              <h3 className="fs-5 text-white">The Lost Girl</h3>
+              <p className="text-white">Quality : HD</p>
+              <Button variant="danger" className="rounded-0 mb-2">
+                Watch Now
+              </Button>
+            </div>
           </div>
         </Link>
       </div>
