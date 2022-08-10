3acf084699855a4df628dd243bbbc3467819e5be
MD. Shimul <mdshemul480@gmail.com>
2022-08-10 16:44:58 +0600

    latest post in home

--- client-side/src/Components/Shared/SinglePost/SinglePost.jsx
+++ client-side/src/Components/Shared/SinglePost/SinglePost.jsx
@@ -4,23 +4,20 @@ import { Link } from "react-router-dom";
 
 import styles from "./SinglePost.module.css";
 
-function SinglePost(props) {
-  // const {
-  //   item: { title, image },
-  // } = props;
+function SinglePost({ item: { title, image, name, quality, watch_time, id } }) {
   return (
     <Col xxl={2}>
       <div className={`rounded ${styles.singlePost_card} p-1 m-1`}>
-        <Link to="/content/2" className="text-decoration-none">
+        <Link to={`/content/${id}`} className="text-decoration-none">
           <div className="overflow-hidden d-flex justify-content-center  align-items-end rounded">
             <img
-              src="https://streamo.vuejstemplate.com/images/product/comedy-series-1.png"
+              src={`${process.env.REACT_APP_IMAGE_FOLDER_LOCATION}/${image}`}
               className={`w-100 ${styles.singlePost_image} rounded`}
               alt=""
             />
             <div className={`text-center ${styles.singlePost_text}`}>
-              <h3 className="fs-5 text-white">The Lost Girl</h3>
-              <p className="text-white">Quality : HD</p>
+              <h3 className="fs-5 text-white">{name}</h3>
+              <p className="text-white">Quality : {quality}</p>
               <Button variant="danger" className="rounded-0 mb-2">
                 Watch Now
               </Button>
