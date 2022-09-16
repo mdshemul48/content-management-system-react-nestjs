ac5d03aeb570a40db3d0734a7d487913cc48b766
MD. Shimul <mdshemul480@gmail.com>
2022-09-16 10:55:50 +0600

    adding form for movie

--- client-side/src/Components/Admin/AddNewPost/AddNewPost.jsx
+++ client-side/src/Components/Admin/AddNewPost/AddNewPost.jsx
@@ -8,9 +8,30 @@ import PosterImage from "./PosterImage/PosterImage";
 
 const AddNewPost = () => {
   const [publishOption, setPublishOption] = useState("movie");
+  const [postDetail, setPostDetail] = useState({
+    title: "",
+    image: null,
+    previewImage: null,
+  });
 
+  console.log(postDetail);
   const onChangeHandler = (event) => {
     setPublishOption(event.target.value);
+  };
+
+  const onImageChangeHandler = (event) => {
+    const file = event.target.files[0];
+    // eslint-disable-next-line no-undef
+    const reader = new FileReader();
+    const url = reader.readAsDataURL(file);
+
+    reader.onloadend = () => {
+      setPostDetail({
+        ...postDetail,
+        image: file,
+        previewImage: reader.result,
+      });
+    };
   };
 
   return (
@@ -21,7 +42,7 @@ const AddNewPost = () => {
           <Col lg={10} md={8}>
             <Form.Group className="mb-3">
               <Form.Label>Title</Form.Label>
-              <Form.Control type="text" />
+              <Form.Control name="title" type="text" />
             </Form.Group>
           </Col>
           <Col lg={2} md={4} className="align-items-end d-flex">
@@ -38,10 +59,43 @@ const AddNewPost = () => {
           </Col>
         </Row>
         <Row>
-          <Col lg={10}>THis is the +</Col>
+          <Col lg={10}>
+            <Row>
+              <Col lg={10}>
+                <Form.Group className="mb-3">
+                  <Form.Label>Name</Form.Label>
+                  <Form.Control name="name" type="text" />
+                </Form.Group>
+              </Col>
+              <Col lg={2}>
+                <Form.Group className="mb-3">
+                  <Form.Label>Year</Form.Label>
+                  <Form.Control name="year" type="text" />
+                </Form.Group>
+              </Col>
+              <Col lg={12}>
+                <Form.Group className="mb-3">
+                  <Form.Label>Download Link</Form.Label>
+                  <Form.Control name="downloadLink" type="text" />
+                </Form.Group>
+              </Col>
+              <Col lg={6}>
+                <Form.Group className="mb-3">
+                  <Form.Label>Watch Time</Form.Label>
+                  <Form.Control name="WatchTime" type="text" />
+                </Form.Group>
+              </Col>
+              <Col lg={6}>
+                <Form.Group className="mb-3">
+                  <Form.Label>Quality</Form.Label>
+                  <Form.Control name="quality" type="text" />
+                </Form.Group>
+              </Col>
+            </Row>
+          </Col>
           <Col lg={2}>
             <Categories />
-            <PosterImage />
+            <PosterImage onImageChangeHandler={onImageChangeHandler} image={postDetail.previewImage} />
           </Col>
         </Row>
       </section>
--- client-side/src/Components/Admin/AddNewPost/PosterImage/PosterImage.jsx
+++ client-side/src/Components/Admin/AddNewPost/PosterImage/PosterImage.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import { Card, Form } from "react-bootstrap";
 import placeHolderImage from "../../../../Assets/300x450.png";
 
-const PosterImage = () => (
+const PosterImage = ({ onImageChangeHandler, image }) => (
   <Card>
     <Card.Header>Poster Image</Card.Header>{" "}
     <Card.Body className="p-1">
       {" "}
-      <img src={placeHolderImage} alt="poster" className="img-fluid mb-1 rounded" />
+      <img src={image || placeHolderImage} alt="poster" className="img-fluid mb-1 rounded" />
       <Form.Group controlId="formFile">
-        <Form.Control type="file" size="sm" />
+        <Form.Control type="file" size="sm" onChange={onImageChangeHandler} />
       </Form.Group>
     </Card.Body>
   </Card>
