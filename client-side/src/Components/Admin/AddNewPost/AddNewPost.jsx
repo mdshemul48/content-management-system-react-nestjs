64db467eb1ed84e75812b9f640e9fc936e41f50d
MD. Shimul <mdshemul480@gmail.com>
2022-09-16 12:09:23 +0600

    adding movie publish page

--- client-side/src/Components/Admin/AddNewPost/AddNewPost.jsx
+++ client-side/src/Components/Admin/AddNewPost/AddNewPost.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
-import { Col, Form, Row } from "react-bootstrap";
+import { Button, ButtonGroup, Card, Col, Form, Row } from "react-bootstrap";
 
 import Categories from "./Categories/Categories";
 
 import styles from "./AddNewPost.module.css";
 import PosterImage from "./PosterImage/PosterImage";
+import Movie from "./Movie/Movie";
 
 const AddNewPost = () => {
   const [publishOption, setPublishOption] = useState("movie");
@@ -12,6 +13,7 @@ const AddNewPost = () => {
     title: "",
     image: null,
     previewImage: null,
+    categories: [],
   });
 
   console.log(postDetail);
@@ -38,66 +40,41 @@ const AddNewPost = () => {
     <main className="m-2 p-3">
       <section>
         <h4>Add New Post</h4>
-        <Row>
-          <Col lg={10} md={8}>
-            <Form.Group className="mb-3">
-              <Form.Label>Title</Form.Label>
-              <Form.Control name="title" type="text" />
-            </Form.Group>
-          </Col>
-          <Col lg={2} md={4} className="align-items-end d-flex">
-            {" "}
-            <Form.Group className="mb-3 w-100">
-              <Form.Select className={styles.selectPublishType} onChange={onChangeHandler}>
-                <option value="movie">Movie</option>
-                <option value="software">Software</option>
-                <option value="game">Game</option>
-                <option value="tutorial">Tutorial</option>
-                <option value="series">Series</option>
-              </Form.Select>
-            </Form.Group>
-          </Col>
-        </Row>
-        <Row>
-          <Col lg={10}>
-            <Row>
-              <Col lg={10}>
-                <Form.Group className="mb-3">
-                  <Form.Label>Name</Form.Label>
-                  <Form.Control name="name" type="text" />
-                </Form.Group>
-              </Col>
-              <Col lg={2}>
-                <Form.Group className="mb-3">
-                  <Form.Label>Year</Form.Label>
-                  <Form.Control name="year" type="text" />
-                </Form.Group>
-              </Col>
-              <Col lg={12}>
-                <Form.Group className="mb-3">
-                  <Form.Label>Download Link</Form.Label>
-                  <Form.Control name="downloadLink" type="text" />
-                </Form.Group>
-              </Col>
-              <Col lg={6}>
-                <Form.Group className="mb-3">
-                  <Form.Label>Watch Time</Form.Label>
-                  <Form.Control name="WatchTime" type="text" />
-                </Form.Group>
-              </Col>
-              <Col lg={6}>
-                <Form.Group className="mb-3">
-                  <Form.Label>Quality</Form.Label>
-                  <Form.Control name="quality" type="text" />
-                </Form.Group>
-              </Col>
-            </Row>
-          </Col>
-          <Col lg={2}>
-            <Categories />
-            <PosterImage onImageChangeHandler={onImageChangeHandler} image={postDetail.previewImage} />
-          </Col>
-        </Row>
+        <Form>
+          <Row>
+            <Col lg={10} md={8}>
+              <Form.Group className="mb-3">
+                <Form.Label>Title</Form.Label>
+                <Form.Control name="title" type="text" />
+              </Form.Group>
+            </Col>
+            <Col lg={2} md={4} className="align-items-end d-flex">
+              {" "}
+              <Form.Group className="mb-3 w-100">
+                <Form.Select className={styles.selectPublishType} onChange={onChangeHandler}>
+                  <option value="movie">Movie</option>
+                  <option value="software">Software</option>
+                  <option value="game">Game</option>
+                  <option value="tutorial">Tutorial</option>
+                  <option value="series">Series</option>
+                </Form.Select>
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Col lg={10}>{publishOption === "movie" && <Movie />}</Col>
+            <Col lg={2}>
+              <Categories />
+              <PosterImage onImageChangeHandler={onImageChangeHandler} image={postDetail.previewImage} />
+              <Card className="p-1 mt-2">
+                {" "}
+                <ButtonGroup className="w-100">
+                  <Button variant="secondary">Back</Button> <Button variant="primary">Submit</Button>
+                </ButtonGroup>
+              </Card>
+            </Col>
+          </Row>
+        </Form>
       </section>
     </main>
   );
