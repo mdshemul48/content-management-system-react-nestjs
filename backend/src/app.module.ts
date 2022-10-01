f8573e65c5a1e34d50491dbb855436c80634075a
MD. Shimul <mdshemul480@gmail.com>
2022-10-01 23:46:49 +0600

    adding static

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,11 +5,16 @@ import { CategoriesModule } from './categories/categories.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { PostsModule } from './posts/posts.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
     }),
     AuthModule,
     UserModule,
