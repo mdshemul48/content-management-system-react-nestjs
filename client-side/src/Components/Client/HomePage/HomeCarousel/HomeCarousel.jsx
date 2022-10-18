69538ebb8fe7e115daf3ffc8c0464c719272ef25
MD. Shimul <mdshemul480@gmail.com>
2022-10-18 13:40:34 +0600

    adding poster to cover area

--- client-side/src/Components/Client/HomePage/HomeCarousel/HomeCarousel.jsx
+++ client-side/src/Components/Client/HomePage/HomeCarousel/HomeCarousel.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Carousel } from "react-bootstrap";
+import { Button, Card, Carousel, Col, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 import styles from "./HomeCarousel.module.css";
@@ -9,7 +9,7 @@ const HomeCarousel = ({ mostPopularPosts }) => (
     {mostPopularPosts.map((post) => (
       <Carousel.Item key={post.id}>
         <img
-          className="d-block w-100"
+          className={`d-block w-100 ${styles.homeCarouselImage}`}
           src={`${process.env.REACT_APP_IMAGE_FOLDER_LOCATION}/${post.cover}`}
           alt="First slide"
           width="1920"
@@ -17,13 +17,30 @@ const HomeCarousel = ({ mostPopularPosts }) => (
         />
         <Link to={`/content/${post.id}`}>
           <Carousel.Caption className={styles.homeCarousel}>
-            <h1 className={`fw-bolder ${styles.homeCarouselTitle}`}>{post.name}</h1>
-            <h2 className="mt-2">
-              {post.categories.find((category) => category.type === "main").name} | {post.watchTime}
-            </h2>
-            <Button variant="danger" className={`rounded-0 mt-3 ${styles.homeCarouselWatchButton}`} size="lg">
-              Watch Now
-            </Button>
+            <Row>
+              <Col lg={8} className={styles.homeCarouselTitle}>
+                <div>
+                  <h1 className={`fw-bolder ${styles.homeCarouselTitle}`}>{post.name}</h1>
+                  <h2 className="mt-2">
+                    {post.categories.find((category) => category.type === "main").name} | {post.watchTime}
+                  </h2>
+                  {post.quality && <h6>{post.quality}</h6>}
+                  <Button variant="danger" className={`rounded-0 mt-3 ${styles.homeCarouselWatchButton}`} size="lg">
+                    Watch Now
+                  </Button>
+                </div>
+              </Col>
+              <Col lg={4}>
+                <div className={`p-1 bg-white d-inline-block rounded ${styles.homeCarouselPoster}`}>
+                  <img
+                    src={`${process.env.REACT_APP_IMAGE_FOLDER_LOCATION}/${post.imageSm}`}
+                    className="rounded img-fluid"
+                    alt=""
+                    max-height="300px"
+                  />
+                </div>
+              </Col>
+            </Row>
           </Carousel.Caption>
         </Link>
       </Carousel.Item>
