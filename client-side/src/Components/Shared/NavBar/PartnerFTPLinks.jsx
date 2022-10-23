211e4c1b5db644f6c7706c184f687205ac073b8a
MD. Shimul <mdshemul480@gmail.com>
2022-10-23 18:21:24 +0600

    adding old ftp link

--- client-side/src/Components/Shared/NavBar/PartnerFTPLinks.jsx
+++ client-side/src/Components/Shared/NavBar/PartnerFTPLinks.jsx
@@ -3,7 +3,7 @@ import { NavDropdown } from "react-bootstrap";
 
 const partnerFTPLinks = () => (
   <NavDropdown title="Partner FTP" id="navbarScrollingDropdown" className="text-uppercase text-dark">
-    <NavDropdown.Item className="text-uppercase" href="http://ftp4.circleftp.net">
+    <NavDropdown.Item className="text-uppercase" href="http://old.circleftp.net">
       OLD FTP
     </NavDropdown.Item>
     <NavDropdown.Item className="text-uppercase" href="http://ftp4.circleftp.net">
