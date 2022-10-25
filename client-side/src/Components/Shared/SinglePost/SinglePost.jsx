3af3bcf8ccb952973fc8a7ea648e9aac8a9bfa83
MD. Shimul <mdshemul480@gmail.com>
2022-10-25 15:01:58 +0600

    fixing single post

--- client-side/src/Components/Shared/SinglePost/SinglePost.jsx
+++ client-side/src/Components/Shared/SinglePost/SinglePost.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 import styles from "./SinglePost.module.css";
 
 function SinglePost({ item }) {
-  const { imageSm, name, quality, id, year, title } = item;
+  const { imageSm, name, quality, id, year, title, type } = item;
   return (
     <Col xxl={2} lg={3} md={4} sm={6}>
       <div className={`rounded ${styles.singlePost_card} p-1 m-1`} title={title}>
@@ -22,8 +22,9 @@ function SinglePost({ item }) {
                 {name} {year && <span>({year})</span>}
               </h3>
               {quality && <p className="text-white">Quality : {quality}</p>}
+
               <Button variant="danger" className="rounded-0 mb-2">
-                Watch Now
+                {type !== "singleFile" && type !== "multiFile" ? "Watch Now" : "Visit Now"}
               </Button>
             </div>
           </div>
